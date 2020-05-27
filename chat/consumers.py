38aacf6e7e5e30e3a569cace80db652a28c89508
Piyush <irock0093@gmail.com>
2020-05-27 14:04:13 +0530

    made a temporary chatbot

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,20 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 
+
+commands = {
+    'name' : 'Hi, Please enter your name. ',
+    'help' : 'How can I help you?', 
+    'contact' : 'Please enter your contact information'
+
+}
+
+
+
+
+
+
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
@@ -8,18 +22,37 @@ class ChatConsumer(WebsocketConsumer):
     def disconnect(self, close_code):
         pass
 
+
+
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        response_message = 'Hello, How can I help you?'
+        
+        if message in commands:
+            if message == 'name':
+                response_message = 'Hi, %s. Nice to meet you. ' %(message)
+
+            if message == 'help':
+                response_message = 'How can I help you?'
+
+            if message == 'contact':
+                response_message = 'Please enter your contact information. '
+
+
+
+        else:
+            response_message = 'Invalid response, please choose from either name, help or contact'
+
         message_parts = message.split()
 
-        response_message = 'You\'ve entered %s' % (message)
+
+        
 
         self.send(text_data=json.dumps({
             'message': response_message,
             'type': 'chat_message'
         }))
+
 
 
 
