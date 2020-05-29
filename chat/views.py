5387df124e4909a087fd0d1fbe2a5f4840bbb056
Piyush <irock0093@gmail.com>
2020-05-29 22:52:58 +0530

    Admin chat working

--- chat/views.py
+++ chat/views.py
@@ -12,7 +12,22 @@ def index(request):
 
 def room(request, room_name):
 	url = '/chat/%s/admin' % (room_name)
-	if url == reverse(room, kwargs = {'room_name' : room_name}):
+	current_url = reverse(room, kwargs = {'room_name' : room_name})
+
+	user_url = 'http://localhost:8000/chat/%s/' %  (room_name)
+	current_url = request.build_absolute_uri()
+
+
+	print('URL is :', current_url)
+	print('user_url is : %s' % (user_url))
+	if user_url == current_url:
+		admin = True
+		print('URL is : %s' % (reverse(room, kwargs = {'room_name' : room_name})))
+		return render(request, 'chat/room.html', {'room_name' : room_name, 'admin' : admin})
+
+
+
+	if url == reverse(room, kwargs = {'room_name' : room_name}) and request.user.is_superuser:
 		admin = True
 
 	else:
